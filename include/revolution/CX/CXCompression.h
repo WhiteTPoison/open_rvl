c186b2fbd6e6b449446f87d7bea598cae0b64053
ilazoja <ilazoja@uwaterloo.ca>
2024-09-04 00:25:07 -0400

    Added CXCompression and CX.h

---
+++ include/revolution/CX/CXCompression.h
@@ -0,0 +1,13 @@
+#ifndef RVL_SDK_CX_COMPRESSION_H
+#define RVL_SDK_CX_COMPRESSION_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static void CXUncompressLZ(void* data, void* destination);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
